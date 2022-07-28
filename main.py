7956cec2b4520da207f8612a34d7ea036f398ca1
Ishaan Sonar <78886258+tancan98@users.noreply.github.com>
2022-07-28 00:11:02 +0000

    fixed problem with name box

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from tkinter import*#for Gui
 import random#for randoniser
 from tkinter import messagebox # for message box
+
+def randomiser():#randomises the questions
+    global qnum#key of the dictionary
+    qnum = random.randint(1,10)#the number of questions
+    if qnum not in asked:#a list is declared, atart with any number will be added
+      asked.append(qnum)
+    elif qnum in asked:
+      randomiser()
+      
 names = []#list
 score=0#list
 asked = []#list
@@ -19,14 +28,6 @@ question_solution = {
     10: ["What month was Prince George born?", 'April','September','May','July','July',4],#Q
 }
 
-def randomiser():#randomises the questions
-    global qnum#key of the dictionary
-    qnum = random.randint(1,10)#the number of questions
-    if qnum not in asked:#a list is declared, atart with any number will be added
-      asked.append(qnum)
-    elif qnum in asked:
-      randomiser()
-      
 # this is my introduction page which greats the user to my quiz
 class IntroductionStarter:
   def __init__(self, parent):
@@ -38,7 +39,7 @@ class IntroductionStarter:
     self.heading_tab=Label(self.quiz_frame, text = "General Knowledge Quiz", font =( "times new roman","18","bold"),bg="gold")
     self.heading_tab.grid(row= 0, padx=20)#heading of the quiz
 
-    self.var1=IntVar()
+    self.constant1=IntVar()
 
     self.user_tab=Label(self.quiz_frame, text="Please Enter your Username Below: ", font=( "times new roman","18","bold"),bg="gold")#username headind
     self.user_tab.grid(row=1, padx=20, pady=20)
@@ -64,7 +65,7 @@ class IntroductionStarter:
                                  'username is not to the required length')
       elif name.isnumeric():
             messagebox.showerror('error has happened!', 'Username can only have letters!')
-      elif not name.replace(' ','').isalpha: # to make sure that letters are entered not number
+      elif not name.replace(' ','').isalpha(): # to make sure that letters are entered not number
         messagebox.showerror('error has happened!', 'No Foreign Symbols! Please Enter Username Again!')
       else:# to make sure letteres are entered not symbols
         name=self.entry_case.get()
@@ -82,7 +83,7 @@ class InstructionsStarter:
     self.heading_tab=Label(self.instructions_frame, text = "Instructions", font =( "times new roman","18","bold"),bg="gold")#main heading
     self.heading_tab.grid(row= 0, padx=20)
 
-    self.var1=IntVar()
+    self.constant1=IntVar()
 
     self.user_tab=Label(self.instructions_frame, text="This is a general knowledge quiz which will \n test your knowledge about \n all topics around the world. Every time you \n get a question right you will have 1 \n point added to your score and \n when you get a question wrong no points will \n be added to your score. Good Luck and Have Fun ",font=( "times new roman","18","bold"),bg="gold")
     self.user_tab.grid(row=1, padx=20, pady=20)#instructions
@@ -97,8 +98,10 @@ class InstructionsStarter:
         Quiz(root)#take user to next page
 
 
+
 # this is where the quiz starts and where the questions will be displayed   
 class Quiz:#Quiz page
+ 
   def __init__(self, parent):
     background_color="black"#background colour
     
@@ -111,19 +114,19 @@ class Quiz:#Quiz page
     self.question_label=Label(self.quiz_frame, text = question_solution [qnum][0], font =(  "times new roman","18","bold"),bg="gold")
     self.question_label.grid(row= 0, padx=10, pady=10)  # question label
 
-    self.var1=IntVar()
+    self.constant1=IntVar()
 
-    self.rb1 = Radiobutton (self.quiz_frame, text = question_solution [qnum][1], font=("times new roman", "12"), bg="gold", value=1, variable=self.var1, pady=10)
-    self.rb1.grid(row=1, sticky=W)#button 1
+    self.variable1 = Radiobutton (self.quiz_frame, text = question_solution [qnum][1], font=("times new roman", "12"), bg="gold", value=1, variable=self.constant1, pady=10)
+    self.variable1.grid(row=1, sticky=W)#button 1
 
-    self.rb2 = Radiobutton (self.quiz_frame, text = question_solution [qnum][2], font=("times new roman", "12"), bg="gold", value=2, variable=self.var1, pady=10)
-    self.rb2.grid(row=2, sticky=W)#button 2
+    self.variable2 = Radiobutton (self.quiz_frame, text = question_solution [qnum][2], font=("times new roman", "12"), bg="gold", value=2, variable=self.constant1, pady=10)
+    self.variable2.grid(row=2, sticky=W)#button 2
 
-    self.rb3 = Radiobutton (self.quiz_frame, text = question_solution [qnum][3], font=("times new roman", "12"), bg="gold", value=3, variable=self.var1, pady=10)
-    self.rb3.grid(row=3, sticky=W)#button 3
+    self.variable3 = Radiobutton (self.quiz_frame, text = question_solution [qnum][3], font=("times new roman", "12"), bg="gold", value=3, variable=self.constant1, pady=10)
+    self.variable3.grid(row=3, sticky=W)#button 3
 
-    self.rb4 = Radiobutton (self.quiz_frame, text = question_solution [qnum][4], font=("times new roman", "12"), bg="gold", value=4, variable=self.var1, pady=10)
-    self.rb4.grid(row=4, sticky=W)#button 4
+    self.variable4 = Radiobutton (self.quiz_frame, text = question_solution [qnum][4], font=("times new roman", "12"), bg="gold", value=4, variable=self.constant1, pady=10)
+    self.variable4.grid(row=4, sticky=W)#button 4
 
     self.confirm_button = Button(self.quiz_frame, text="Confrim",bg="gold",command=self.trial_progression)
     self.confirm_button.grid(row=6)#confirm button
@@ -141,18 +144,18 @@ class Quiz:#Quiz page
      
   def questions_system(self):
      randomiser()
-     self.var1.set(0)
+     self.constant1.set(0)
      self.question_label.config(text=question_solution [qnum][0])
-     self.rb1.config(text=question_solution [qnum][1])
-     self.rb2.config(text=question_solution [qnum][2])
-     self.rb3.config(text=question_solution [qnum][3])
-     self.rb4.config(text=question_solution [qnum][4])
+     self.variable1.config(text=question_solution [qnum][1])
+     self.variable2.config(text=question_solution [qnum][2])
+     self.variable3.config(text=question_solution [qnum][3])
+     self.variable4.config(text=question_solution [qnum][4])
 
 #this is the score system that tells the users what their score is
   def trial_progression(self):
       global score 
       scr_tab=self.score_label
-      choice=self.var1.get()
+      choice=self.constant1.get()
       if len(asked)>9: #  finds out the last question of the quiz
         if choice == question_solution [qnum][6]: # checks that the key is correct and stores the answer in index 6
           score +=1 # one point is added to score
@@ -168,7 +171,7 @@ class Quiz:#Quiz page
       else:
             if choice==0:
               self.confirm_button.config(text=" Please Try Again, you didn't select an answer then submit again" )#when the user has no selected the option this message shows up
-              choice=self.var1.get()
+              choice=self.constant1.get()
             else:
               if choice == question_solution [qnum][6]:
                 score+=1
@@ -186,11 +189,11 @@ class Quiz:#Quiz page
   def end_window(self):
     root.destroy()#destroys quiz page
     name = names[0]
-    open_end_object = End()#opens end page
+    open_end_object = EndStarter()#opens end page
 
 
 
-class End:# End page
+class EndStarter:# End page
 
 
   def __init__(self):
